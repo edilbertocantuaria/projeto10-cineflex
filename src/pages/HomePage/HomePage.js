cbc6cf2fa31ea9a229a5d7c81a90ce293f7c4267
edilbertocantuaria <edilbertounbfga@gmail.com>
2023-03-12 12:21:46 -0300

    Puxando as imagens da API - homepage

---
+++ src/pages/HomePage/HomePage.js
@@ -0,0 +1,63 @@
+import styled from "styled-components"
+import axios from "axios"
+import { useState, useEffect} from "react"
+
+export default function HomePage() {
+const [movies, setMovies]= useState([]);
+
+useEffect(()=>{
+    const request = axios.get("https://mock-api.driven.com.br/api/v8/cineflex/movies");
+    request.then(response => {
+        setMovies(response.data);
+        console.log(movies);
+    })
+
+},[])
+
+
+    return (
+        <PageContainer>
+            Selecione o filme
+
+            <ListContainer>
+                
+                    {movies.map(movie => <MovieContainer><img src={movie.posterURL} alt="poster" key={movie.id}/> </MovieContainer>)}
+               
+            </ListContainer>
+
+        </PageContainer>
+    )
+}
+
+const PageContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    font-family: 'Roboto';
+    font-size: 24px;
+    text-align: center;
+    color: #293845;
+    margin-top: 30px;
+    padding-top: 70px;
+`
+const ListContainer = styled.div`
+    width: 330px;
+    display: flex;
+    flex-wrap: wrap;
+    flex-direction: row;
+    padding: 10px;
+`
+const MovieContainer = styled.div`
+    width: 145px;
+    height: 210px;
+    box-shadow: 0px 2px 4px 2px #0000001A;
+    border-radius: 3px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin: 10px;
+    img {
+        width: 130px;
+        height: 190px;
+    }
+`
